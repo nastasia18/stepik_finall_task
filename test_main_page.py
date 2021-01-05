a6917d5c9e905f5596aa6bc6191fbadf7abc8bbe
Anastasia Zavarina <staszavar.2017@gmail.com>
2021-01-05 16:45:00 +0300

    Реализация LoginPage

--- test_main_page.py
+++ test_main_page.py
@@ -1,9 +1,31 @@
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 
 
+def test_guest_can_go_to_login_url(browser):
+    link = "http://selenium1py.pythonanywhere.com/accounts/login/"
+    page = LoginPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page.open()  # открываем страницу
+    page.should_be_login_url()
+
+
+def test_guest_can_go_to_login_form(browser):
+    link = "http://selenium1py.pythonanywhere.com/accounts/login/"
+    page = LoginPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page.open()  # открываем страницу
+    page.should_be_login_form()
+
+
+def test_guest_can_go_to_register_form(browser):
+    link = "http://selenium1py.pythonanywhere.com/accounts/login/"
+    page = LoginPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page.open()  # открываем страницу
+    page.should_be_register_form()
+
+"""
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()  # открываем страницу
-    # page.go_to_login_page()  # выполняем метод страницы — переходим на страницу логина
-    page.should_be_login_link()
+    page.should_be_login_url()
+"""
