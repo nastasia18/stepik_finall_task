1c7c4ae5754427c886b85fb36f5c9cc11bb8c5ed
Anastasia Zavarina <staszavar.2017@gmail.com>
2021-01-05 13:02:54 +0300

    Перепись теста из файла test_main_page.py с помощью паттерна Page Object

--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,9 @@
+def can_go_to_login_page(browser):
+    login_link = browser.find_element_by_css_selector("#login_link")
+    login_link.click()
+
+
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     browser.get(link)
-    login_link = browser.find_element_by_css_selector("#login_link")
-    login_link.click()
+    can_go_to_login_page(browser)
