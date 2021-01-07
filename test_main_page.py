0c51b77661e668b4d18c3ea4b5247ece7403f3a8
Anastasia Zavarina <staszavar.2017@gmail.com>
2021-01-07 14:11:19 +0300

    Группировка тестов

--- test_main_page.py
+++ test_main_page.py
@@ -4,13 +4,6 @@ from .pages.basket_page import BasketPage
 import pytest
 
 
-def test_guest_can_go_to_login_url(browser):
-    link = "http://selenium1py.pythonanywhere.com/accounts/login/"
-    page = LoginPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-    page.open()  # открываем страницу
-    page.should_be_login_url()
-
-
 def test_guest_can_go_to_login_form(browser):
     link = "http://selenium1py.pythonanywhere.com/accounts/login/"
     page = LoginPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
@@ -25,15 +18,6 @@ def test_guest_can_go_to_register_form(browser):
     page.should_be_register_form()
 
 
-def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-    page.open()  # открываем страницу
-    page.go_to_login_page()
-    login_page = LoginPage(browser, browser.current_url)
-    login_page.should_be_login_page()
-
-
 def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = BasketPage(browser, link)
@@ -41,6 +25,24 @@ def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
     page.go_to_basket()
     page.should_not_be_product_in_basket()
     page.should_text_not_be_product_in_basket()
+
+
+@pytest.mark.login_guest
+class TestLoginFromMainPage:
+    def test_guest_can_go_to_login_page(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/"
+        page = MainPage(browser, link)
+        page.open()  # открываем страницу
+        page.go_to_login_page()
+        login_page = LoginPage(browser, browser.current_url)
+        login_page.should_be_login_page()
+
+    def test_guest_can_go_to_login_url(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/accounts/login/"
+        page = LoginPage(browser, link)
+        page.open()  # открываем страницу
+        page.should_be_login_url()
+
 
 
 
