2a9d3b20455098ac61f97a0a38de42507e2c202b
Anastasia Zavarina <staszavar.2017@gmail.com>
2021-01-07 12:19:41 +0300

    Добавление отрицательных проверок

--- pages/product_page.py
+++ pages/product_page.py
@@ -9,7 +9,6 @@ class ProductPage(BasePage):
         btn = self.browser.find_element(*ProductPageLocators.BUTTON)
         btn.click()
 
-
     def should_be_button(self):
         # проверка, что есть кнопка Добавить в корзину
         assert self.is_element_present(*ProductPageLocators.BUTTON), "Button 'add to basket' is not presented"
@@ -18,7 +17,7 @@ class ProductPage(BasePage):
         # проверка корзины
         self.should_be_product_name()
         self.should_be_price()
-        self.should_be_message()
+        self.should_be_success_message()
 
     def should_be_product_name(self):
         # проверка, что название книги соответствующее
@@ -32,8 +31,18 @@ class ProductPage(BasePage):
         price_basket = self.browser.find_element(*ProductPageLocators.PRICE_BASKET)
         assert price.text == price_basket.text, "Price does not match"
 
-    def should_be_message(self):
+    def should_be_success_message(self):
         # проверка, что есть сообщение о том, что товар добавлен в корзину
         message = self.browser.find_element(*ProductPageLocators.MESSAGE)
         title_book_basket = self.browser.find_element(*ProductPageLocators.TITLE_BOOK_BASKET)
         assert title_book_basket.text in message.text, "Message is not presented"
+
+    def should_not_be_success_message(self):
+        # проверка, что нет сообщения о том, что товар добавлен в корзину
+        assert self.is_not_element_present(*ProductPageLocators.MESSAGE), \
+            "Success message is presented, but should not be"
+
+    def should_is_disappeared_success_message(self):
+        # проверка, что сообщение исчезает через какое-то время
+        assert self.is_disappeared(*ProductPageLocators.MESSAGE), \
+            "Success message is not disappeared, but should not be"
